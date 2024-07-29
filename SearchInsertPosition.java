fd2d0166f9023d2845d19e43f40e2f3059c8425b
Kishu Kumar <149893082+kishukumar091@users.noreply.github.com>
2024-07-29 22:52:55 +0530

    Create SearchInsertPosition.java

---
+++ SearchInsertPosition.java
@@ -0,0 +1,15 @@
+int start =0;
+int end=nums.length-1;
+while(start<=end){
+  int mid=(start+end)/2;
+  if(nums[mid]==target){
+      return mid;
+  }
+  if (nums[mid]<target){
+     start=mid+1;
+ }
+  else{
+     end=mid-1;
+  }
+}
+return start;
