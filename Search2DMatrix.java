3d70061d9eb0da9fb96375f09047ef186801375b
Kishu Kumar <149893082+kishukumar091@users.noreply.github.com>
2024-07-31 16:10:24 +0530

    Add files via upload

---
+++ Search2DMatrix.java
@@ -0,0 +1,12 @@
+public class Search2DMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (target == matrix[row][col]) return true;
+            else if (target > matrix[row][col]) row++;
+            else col--;
+        }
+        return false;
+    }
+}
