44f9472d2f94cfd43889742ebe172d5b168a8bf6
Kishu Kumar <149893082+kishukumar091@users.noreply.github.com>
2024-09-15 18:39:41 +0530

    Add files via upload

---
+++ Stack_LinkedList.java
@@ -0,0 +1,57 @@
+public class Stack_LinkedList {
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    static class Stack{
+        static Node head = null;
+        public static boolean isEmpty(){
+            return head==null;
+        }
+
+        //push
+        public static void push(int data){
+            Node newNode = new Node(data);
+            if(isEmpty()){
+                head = newNode;
+            } else {
+                newNode.next = head;
+                head = newNode;
+            }
+        }
+
+        //pop
+        public static int pop() {
+            int result;
+            if (isEmpty()) {
+                result = -1;
+            } else {
+                int top = head.data;
+                head = head.next;
+                result = top;
+            }
+            return result;
+        }
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+            return head.data;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s=new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
